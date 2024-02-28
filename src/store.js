75b9e20777dceb56986b9e8d694cbf3a4e64b11f
Rachit Jain <rachitjain9596@gmail.com>
2024-02-29 01:20:57 +0530

    accountSlice

--- src/store.js
+++ src/store.js
@@ -1,45 +1,10 @@
 import { combineReducers, createStore } from 'redux';
 
-const initialStateAccount = {
-  balance: 0,
-  loan: 0,
-  loanPurpose: '',
-};
-
 const initialStateCustomer = {
   fullName: '',
   nationalId: '',
   createdAt: '',
 };
-
-function accountReducer(state = initialStateAccount, action) {
-  switch (action.type) {
-    case 'account/deposit':
-      return { ...state, balance: state.balance + action.payload };
-
-    case 'account/withdraw':
-      return { ...state, balance: state.balance - action.payload };
-
-    case 'account/requestLoan':
-      if (state.loan > 0) return state;
-      return {
-        ...state,
-        loan: state.loan + action.payload.amount,
-        loanPurpose: action.payload.purpose,
-        balance: state.balance + action.payload.amount,
-      };
-
-    case 'account/payLoan':
-      return {
-        ...state,
-        balance: state.balance - state.loan,
-        loan: 0,
-        loanPurpose: '',
-      };
-    default:
-      return state;
-  }
-}
 
 function customerReducer(state = initialStateCustomer, action) {
   switch (action.type) {
@@ -79,22 +44,6 @@ const store = createStore(rootReducer);
 
 // store.dispatch({ type: 'account/payLoan' });
 // console.log(store.getState());
-
-function deposit(amount) {
-  return { type: 'account/deposit', payload: amount };
-}
-function withdraw(amount) {
-  return { type: 'account/withdraw', payload: amount };
-}
-function requestLoan(amount) {
-  return {
-    type: 'account/requestLoan',
-    payload: { amount: amount, purpose: 'car' },
-  };
-}
-function payLoan() {
-  return { type: 'account/payLoan' };
-}
 
 store.dispatch(deposit(500));
 console.log(store.getState());
