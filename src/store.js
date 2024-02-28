752dd8b8c17f3968c8fdbcf717f7ec6991a43167
Rachit Jain <rachitjain9596@gmail.com>
2024-02-29 01:22:47 +0530

    customerSlice

--- src/store.js
+++ src/store.js
@@ -1,76 +1,8 @@
 import { combineReducers, createStore } from 'redux';
 
-const initialStateCustomer = {
-  fullName: '',
-  nationalId: '',
-  createdAt: '',
-};
-
-function customerReducer(state = initialStateCustomer, action) {
-  switch (action.type) {
-    case 'customer/createCustomer':
-      return {
-        ...state,
-        fullName: action.payload.fullName,
-        nationalId: action.payload.nationalId,
-        createdAt: action.payload.createdAt,
-      };
-    case 'customer/updateName':
-      return { ...state, function: action.payload };
-
-    default:
-      return state;
-  }
-}
-
 const rootReducer = combineReducers({
   account: accountReducer,
   customer: customerReducer,
 });
 
 const store = createStore(rootReducer);
-
-// store.dispatch({ type: 'account/deposit', payload: 500 });
-// console.log(store.getState());
-
-// store.dispatch({ type: 'account/withdraw', payload: 200 });
-// console.log(store.getState());
-
-// store.dispatch({
-//   type: 'account/requestLoan',
-//   payload: { amount: 1500, purpose: 'car' },
-// });
-// console.log(store.getState());
-
-// store.dispatch({ type: 'account/payLoan' });
-// console.log(store.getState());
-
-store.dispatch(deposit(500));
-console.log(store.getState());
-
-store.dispatch(withdraw(200));
-console.log(store.getState());
-
-store.dispatch(requestLoan(1500));
-console.log(store.getState());
-
-store.dispatch(payLoan());
-console.log(store.getState());
-
-function createCustomer(fullName, nationalId) {
-  return {
-    type: 'customer/createCustomer',
-    payload: {
-      fullName,
-      nationalId,
-      createdAt: new Date().toISOString(),
-    },
-  };
-}
-
-function updateName(fullName) {
-  return { type: 'customer/updateName', payload: fullName };
-}
-
-store.dispatch(createCustomer('ronny', '8452'));
-console.log(store.getState());
